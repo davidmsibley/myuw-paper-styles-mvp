4c7db7524ee3430adc315ad01d09f4e4854f9965
David M Sibley <sibley@wisc.edu>
2018-07-30 19:16:36 -0500

    Updates

--- src/appendHead.js
+++
@@ -1,8 +0,0 @@
-export function appendHead(tpl) {
-  // Prepare stylesheet of custom CSS properties
-  const tplElement = document.createElement('template');
-  tplElement.innerHTML = tpl;
-
-  // Add templates to the document
-  document.head.appendChild(tplElement.content);
-}
--- src/madison-theme.js
+++
@@ -1,8 +0,0 @@
-import { appendHead } from './appendHead.js';
-import commonTpl from './myuw-common-vars.html';
-import colorsTpl from './myuw-colors-madison.html';
-import themeTpl from './madison-theme.html';
-
-appendHead(commonTpl);
-appendHead(colorsTpl);
-appendHead(themeTpl);
